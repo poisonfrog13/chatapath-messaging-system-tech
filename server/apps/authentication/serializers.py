c158d9d78f67e9605edb7fb060b63a21eeb8961e
Olena Gut <lena.gut1993@gmail.com>
2024-05-04 18:42:42 +0100

    Create an initial version of serializers for ChatUser model

---
+++ server/apps/authentication/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from server.apps.authentication.models import ChatUser
+from phonenumber_field.serializerfields import PhoneNumberField
+
+
+class CreateChatUserSerializer(serializers.ModelSerializer):
+    """To deserialize from JSON into ChatUser"""
+
+    class Meta:
+        model = ChatUser
+        fields = ("username", "email", "password", "phone_number")
+
+    phone_number = PhoneNumberField()
+
+
+class ChatUserSerializer(serializers.ModelSerializer):
+    """To serialize from ChatUser to JSON"""
+
+    class Meta:
+        model = ChatUser
+        fields = ("id", "username", "phone_number")
+
+    phone_number = PhoneNumberField()
---
+++ server/apps/chatapath/serializers.py
@@ -0,0 +1,3 @@
+from rest_framework import serializers
+
+from server.apps.authentication.serializers import ChatUserSerializer
